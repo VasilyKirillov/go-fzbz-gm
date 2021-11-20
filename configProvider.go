c6e11f440403aa073358c3eef8686bc192940394
Vasilii Kirillov <vasilii_kirillov@epam.com>
2021-11-20 19:27:17 +0400

    hotfix1

--- configProvider.go
+++ configProvider.go
@@ -7,10 +7,10 @@ import (
 )
 
 const (
-	LOW_PROMPT  = "Please input where to start counting the fizzBuzz numbers (upper boundry):"
-	UP_PROMPT   = "Please input where to stop counting the fizzBuzz numbers (lower boundry):"
-	LOW_ERR_MSG = "Wrong lower boundry"
-	UP_ERR_MSG  = "Wrong upper boundry"
+	LOW_PROMPT  = "Please input where to start counting the fizzBuzz numbers (lower boundry):"
+	UP_PROMPT   = "Please input where to stop counting the fizzBuzz numbers (upper boundry):"
+	LOW_ERR_MSG = "Wrong lower boundry:"
+	UP_ERR_MSG  = "Wrong upper boundry:"
 )
 
 type FizzBuzzConfiger interface {
@@ -33,24 +33,26 @@ func (c *FizzBuzzConfig) getLower() int {
 func (c *FizzBuzzConfig) promptRange() {
 	reader := bufio.NewReader(os.Stdin)
 
-	for {
-		l_text := readNumberStr(reader, LOW_PROMPT, LOW_ERR_MSG)
-		c.l = parseInt(l_text, LOW_ERR_MSG)
-
-		u_text := readNumberStr(reader, UP_PROMPT, UP_ERR_MSG)
-		c.u = parseInt(u_text, UP_ERR_MSG)
-
-		if r := recover(); r == nil {
-			break
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r, "Try one more time!")
+			c.promptRange()
 		}
-	}
+	}()
+
+	l_text := readNumberStr(reader, LOW_PROMPT, LOW_ERR_MSG)
+	c.l = parseInt(l_text, LOW_ERR_MSG)
+
+	u_text := readNumberStr(reader, UP_PROMPT, UP_ERR_MSG)
+	c.u = parseInt(u_text, UP_ERR_MSG)
+
 }
 
 func readNumberStr(reader *bufio.Reader, prompt string, errMsg string) (text string) {
 	fmt.Println(prompt)
 	text, err := reader.ReadString('\n')
 	if err != nil {
-		panic(errMsg + text)
+		panic(fmt.Sprintf("%s %s", errMsg, text))
 	}
 	return
 }
@@ -59,7 +61,7 @@ func parseInt(text string, errMsg string) int {
 	var n int
 	_, err := fmt.Sscan(text, &n)
 	if err != nil {
-		panic(errMsg + text)
+		panic(fmt.Sprintf("%s %s", errMsg, text))
 	}
 	return n
 }
